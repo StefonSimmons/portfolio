221e74bc63298e4e1454c0ddd7c36ea198f57095
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-18 23:58:17 -0400

    fixed some webpack conflicts from installs

--- portfolio_app/src/components/AboutMe.jsx
+++ portfolio_app/src/components/AboutMe.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 const AboutContainer = styled.main`
   border: green solid 3px;
   display: flex;
   justify-content: space-around;
   position: relative;
   top: -3vh;
-`
-const About = styled.section`
-border: orange solid 3px;
+  font-family: 'Ubuntu Condensed', sans-serif;
 
 `
-const Technical = styled.section`
-border: blue solid 3px;
+const AboutSection = styled.section`
+  border: orange solid 3px;
+  margin: 0 25px;
+  width: 300px;
+  padding: 15px;
+  letter-spacing: 1.5px;
+  line-height: 1.5;
+`
+
+const Statement = styled.p`
+
+`
+const TechSection = styled.section`
+  border: blue solid 3px;
+  margin: 0 25px;
+  padding: 15px;
 
 `
 const Header = styled.h1`
-  font-family: 'Ubuntu Condensed', sans-serif;
-  font-size: 28px;
-  letter-spacing: 2px;
+  margin-bottom: 15px;
+  text-align: center;
+  font-size: 32px;
+  letter-spacing: 3px;
   text-decoration: underline;
   line-height: 1.6;
 `
@@ -28,13 +42,16 @@ export default function AboutMe() {
   return (
     <>
       <AboutContainer>
-        <About>
+
+        <AboutSection>
           <Header>About Me</Header>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore inventore non eius fugit qui, iusto magni nemo nisi blanditiis aperiam sequi reprehenderit minima pariatur quos ad sit accusamus aliquam voluptates.</p>
-        </About>
-        <Technical>
-          <Header>Technical Utility Belt</Header>
-        </Technical>
+          <Statement>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore inventore non eius fugit qui, iusto magni nemo nisi blanditiis aperiam sequi reprehenderit minima pariatur quos ad sit accusamus aliquam voluptates.</Statement>
+        </AboutSection>
+
+        <TechSection>
+          <Header>Technical Utility Belt</Header>        
+        </TechSection>
+
       </AboutContainer>
       
     </>
