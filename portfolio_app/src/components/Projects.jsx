4a285e82fc5c7b1a1c2ba625e28f10108e91c2b0
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-25 09:54:58 -0400

    flex on project containers

--- portfolio_app/src/components/Projects.jsx
+++ portfolio_app/src/components/Projects.jsx
@@ -7,15 +7,18 @@ import styled from 'styled-components'
 const Main = styled.main`
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
   margin-bottom: 60px
 `
 const Prj = styled.div`
+  display: flex;
+  flex-direction: column;
+  jusitfy-content: flex-end;
   background: #e8eddf; 
   width: 350px;
   padding: 15px 35px;
   margin: 0px 35px 50px 35px;
   border-radius: 10px;
-  box-shadow: 5px 10px 20px rgb(20,20,20);
   text-align: center;
   letter-spacing: 3px;
   font-family: 'Ubuntu Condensed', sans-serif;
@@ -33,7 +36,6 @@ const Description = styled.p`
   font-size: 18px;
   text-align: left;
   line-height: 1.5;
-  margin-bottom: 15px
 `
 const List = styled.ul`
   text-align: left;
@@ -42,8 +44,8 @@ const List = styled.ul`
   column-gap: 9px
 `
 const HeaderTools = styled.h6`
-  font-size: 18px;
-  text-align: left;
+  font-size: 20px;
+  text-align: center;
   font-weight: 900;
   text-decoration: underline;
   margin-bottom: 10px;
@@ -53,6 +55,32 @@ const Tech = styled.li`
   line-height: 1.5;
   font-size: 16px;
 `
+const SiteContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: auto 0
+
+`
+const Site = styled.a`
+  text-decoration: none;
+  font-weight: 900;
+  font-size: 20px;
+  color: black;
+  border: solid 1px grey;
+  border-radius: 5px;
+  padding: 5px 15px;
+  align-self: flex-end;
+  margin: 35px;
+
+  &:hover{
+    background: #c6cbbd;  
+  }
+`
+const Divider = styled.hr`
+  margin: 15px;
+  width: 100%
+`
 export default function Projects() {
 
   const projects = project.map((p, id) => {
@@ -61,16 +89,18 @@ export default function Projects() {
         <PrjName>{p.name}</PrjName>
         <Thumbnail src={p.image} alt={p.name} />
         <Description>{p.description}</Description>
-        <hr />
+        <Divider />
         <HeaderTools>Tech</HeaderTools>
         <List>
           {p.tech.map(tech =>
             <Tech>{tech}</Tech>
           )}
         </List>
-        <hr />
-        <a href={p.deployedURL}>Live</a>
-        <a href={p.ghRepoURL}>GH</a>
+        <Divider />
+        <SiteContainer>
+          <Site href={p.deployedURL}>Live</Site>
+          <Site href={p.ghRepoURL}>Github</Site>
+        </SiteContainer>
       </Prj>
     )
   })
