89a540cb552e899ad78a8338bf212a076a0d4d95
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 10:28:30 -0400

    updated experience section of resume

--- portfolio_app/src/components/Resume/BrandStatement.js
+++ portfolio_app/src/components/Resume/BrandStatement.js
@@ -4,8 +4,7 @@ export default function BrandStatement() {
   return (
     <>
       <p>
-        I’m a self-motivated creator with over 6 years of experience working within higher education administration as a College Registrar and 15 years as a recording artist. My creativity as an artist and organization as an administrator feed greatly into my passion for software development. Currently, I am seeking to work with people and companies who, like myself, are determined to learn and develop others and their products/services. I consistently contribute strong problem-solving and interpersonal skills as well as an ad/aptive mindset for new processes and technology that may increase productivity.
-      </p>
+      I’m a self-motivated creator with over 6 years of experience working as a higher education professional and 15 years as a recording artist. My creativity as an artist and organization as an administrator feed greatly into my passion for software development. Currently, I am seeking to work with people and companies who, like myself, are determined to learn and develop products/services as well as people. I consistently contribute strong problem-solving and interpersonal skills to my work and rely on my adaptive mindset for designing new processes and technology to production.      </p>
     </>
   )
 }
--- portfolio_app/src/data/experience-data.js
+++ portfolio_app/src/data/experience-data.js
@@ -2,14 +2,23 @@
   // title, company, start date, end date, an array of items
   // the array comes from a paragraph parsed by a period '.'
 
+const softwareEngTAPhrases =
+  "Analyze and debug code produced by a team of up to 30 web developers in an online remote learning environment; Foster a results-driven community for web development that encourages well-documented and efficient production code; Teach web developers the basic implementation of HTML, CSS, JavaScript, React, Ruby on Rails, MongoDB and SQL"
+
 const softwareEngPhrases =
   "Successfully designed, developed, and deployed 4 responsive web applications within strict one-week due dates; Dedicated over 800 hours in 3 months towards developing my skills in planning and deploying web applications; Leveraged fluent knowledge of HTML, CSS, JavaScript, React, Ruby, MongoDB and API calls in order to build well-documented professional and dynamic full-stack web applications"
 
 const registrarPhrases = 
-  "Created a system that consolidates the time it takes to track and report attendance for an average annual enrollment of 270 students by using MS Excel formulas, Word, SQL Server and SQL Reporting tools;Designed the student and faculty-facing student and faculty portal using HTML and in-line CSS;Developed an interdepartmental Excel workbook via OneDrive that calculates and forecasts an average of $100,000 per year in expected veteran educational payments to the school;Optimized student enrollment data collection procedures for productive analysis and reporting to accreditors as well as institutional, state and federal governing bodies;Trained students, staff and faculty on how to navigate and use the student portal and student information system;Resolved technical issues for students, faculty and staff to maintain production of services;Advised a diverse faculty and student body regarding academic options and requirements;Pioneered the implementation of NSC eTranscript and Student Tracker services for the institution"
+  "Created a system that consolidates the time it takes to track and report attendance for an average annual enrollment of 270 students by using MS Excel formulas, Word, SQL Server and SQL Reporting tools;Designed the student and faculty-facing student and faculty portal using HTML and in-line CSS;Developed an interdepartmental Excel workbook via OneDrive that calculates and forecasts an average of $100,000 per year in expected veteran educational payments to the school;Optimized student enrollment data collection procedures for productive analysis and reporting to accreditors as well as institutional, state and federal governing bodies; Resolved technical issues for students, faculty and staff to maintain production of services; Pioneered the implementation of NSC eTranscript and Student Tracker services for the institution"
 
 
 const experience = [{
+  title: "software engineering teaching assistant",
+  business: "GENERAL ASSEMBLY",
+  startDate: "July 2020",
+  endDate: "Present",
+  phrases: softwareEngTAPhrases
+}, {
   title: "software engineering fellow",
   business: "GENERAL ASSEMBLY",
   startDate: "March 2020",
