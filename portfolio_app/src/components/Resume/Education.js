f7cb6a24da8ddbd45060e03db5e963cbe21ef7c1
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 10:48:15 -0400

    updated education

--- portfolio_app/src/components/Resume/Education.js
+++ portfolio_app/src/components/Resume/Education.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DetailsHeader} from './Resume'
+import { DetailsHeader } from './Resume'
 import { Block, Dates, FlexBlock, List, Phrase } from './Experience'
 import styled from 'styled-components'
 import educationData from '../../data/education-data.json'
@@ -12,25 +12,30 @@ const Confer = styled.h2`
 `
 export default function Education() {
 
-  const education = educationData.map(( ed, id ) => {
+  const education = educationData.map((ed, id) => {
     return (
       <Block key={id}>
         <FlexBlock>
-          <DetailsHeader>{ed.institution},</DetailsHeader>
-          <Location>{ed.location}</Location>
+          <DetailsHeader>{ed.institution}</DetailsHeader>
+          {ed.location ? <Location>{ed.location}</Location> :
+            null
+          }
         </FlexBlock>
-      <List>
-        <Phrase>{ed.major}</Phrase>
+        <List>
+          <Phrase>{ed.major} | {ed.award}, {ed.conferralDate}</Phrase>
+          {ed.major2 ? <Phrase>{ed.major2} | {ed.award2}, {ed.conferralDate2}</Phrase> :
+            null
+          }
+          {ed.major3 ? <Phrase>{ed.major3} | {ed.award3}, {ed.conferralDate3}</Phrase> :
+            null
+          }
+
           {ed.GPA ?
             <Phrase>{ed.GPA}</Phrase> :
             null
           }
-        <FlexBlock>
-          <Phrase>{ed.award},</Phrase>
-          <Confer>{ed.conferralDate}</Confer>
-        </FlexBlock>
-      </List>
-    </Block>
+        </List>
+      </Block>
     )
   })
 
