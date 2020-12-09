afaeb31d018ee28a15771b6956fca76f9887b0a7
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-12-08 21:57:14 -0500

    added painting

---
+++ portfolio_app/src/components/MyArt/Music.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import CDDescription from './CDDescription'
+
+import styled from 'styled-components'
+import { ArtContainer } from './MyArt'
+
+const ArtSection = styled.section`
+  padding: 10px 15px 0 15px;
+  letter-spacing: 1.5px;
+  line-height: 1.5;
+`
+const HeaderTwo = styled.h2`
+  margin: 15px 0px;
+  text-align: center;
+  font-size: 24px;
+  letter-spacing: 3px;
+  line-height: 1.6;
+  font-family: 'Ubuntu Condensed', sans-serif;
+  color: rgb(82,150,250)
+`
+const AppreciatedEP = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 20px;
+  color: #e8eddf
+`
+const EPArt = styled.img`
+  width: 400px;
+
+  @media(max-width: 415px){
+    width: 300px;
+  }
+`
+const StoreSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 25px;
+`
+const StoreLnk = styled.a`
+  margin: 10px 20px;
+`
+const StoreLogo = styled.img`
+  border-radius: 10px;
+  width: 200px;
+  transition: transform .5s;
+
+  &:hover{
+    transform: scale(1.25)
+  }
+`
+const ListingSection = styled.section`
+  margin: 1em 0 8em 0;
+  border-radius: 10px;
+  padding: 5px;
+`
+const ToolBelt = styled.div`
+  display: flex;
+  
+  @media(max-width: 770px){
+    flex-direction: column;
+  }
+`
+const CDCover = styled.img`
+  width: 300px;
+`
+const DividerArt = styled.hr`
+  margin: 15px;
+  width: 8%
+`
+export default function Music() {
+  return (
+    <ArtContainer>
+      <ArtSection>
+        <HeaderTwo>Listen to my "Appreciated" EP</HeaderTwo>
+        <AppreciatedEP>
+          <EPArt src='https://i.imgur.com/chpYvXn.png' alt='appreciated ep' />
+
+          <StoreSection>
+            <StoreLnk href='https://itunes.apple.com/us/album/appreciated-ep/id1238965226' target='_blank' rel='noopener noreferrer'>
+              <StoreLogo src="https://imgur.com/jeQ6reJ.png" alt="itunes" />
+            </StoreLnk>
+            <StoreLnk href='https://listen.tidal.com/album/74248583' target='_blank' rel='noopener noreferrer'>
+              <StoreLogo src="https://imgur.com/DoMBglI.png" alt="tidal" />
+            </StoreLnk>
+            <StoreLnk href='https://open.spotify.com/album/7zEyKnrn9ZbcL08pWDRavb' target='_blank' rel='noopener noreferrer'>
+              <StoreLogo src="https://imgur.com/k3G2C5e.png" alt="spotify" />
+            </StoreLnk>
+            <StoreLnk href='https://play.google.com/store/music/album/Stefon_Simmons_Appreciated?id=Bi6k4veuseh4xvacyfia2smp45e' target='_blank' rel='noopener noreferrer'>
+              <StoreLogo src="https://imgur.com/6XTu30o.png" alt="google play" />
+            </StoreLnk>
+            <StoreLnk href='https://amzn.to/2YMeF6p' target='_blank' rel='noopener noreferrer'>
+              <StoreLogo src="https://imgur.com/GJpA6K5.png" alt="amazon music" />
+            </StoreLnk>
+          </StoreSection>
+
+        </AppreciatedEP>
+
+      </ArtSection>
+      <DividerArt />
+
+      <ListingSection>
+        <HeaderTwo>Appreciated Track Listing</HeaderTwo>
+        <ToolBelt>
+          <CDCover src='https://imgur.com/AhVFued.png' alt='appreciated front cover' />
+          <CDCover src='https://imgur.com/9LVYiwt.png' alt='appreciated back cover' />
+          <CDDescription />
+        </ToolBelt>
+      </ListingSection>
+    </ArtContainer>
+  )
+}
--- portfolio_app/src/components/MyArt/MyArt.jsx
+++ portfolio_app/src/components/MyArt/MyArt.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import Music from './Music'
+import Painting from './Painting'
+
 import styled from 'styled-components'
-import CDDescription from './CDDescription'
 
-
-const ArtContainer = styled.main`
+export const ArtContainer = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   font-family: 'Ubuntu Condensed', sans-serif;
 `
-const ArtSection = styled.section`
-  padding: 10px 15px 0 15px;
-  letter-spacing: 1.5px;
-  line-height: 1.5;
-`
+
 const Header = styled.h1`
   margin: 15px 0px;
   text-align: center;
@@ -24,69 +21,29 @@ const Header = styled.h1`
   font-family: 'Ubuntu Condensed', sans-serif;
   color: rgb(82,150,250)
 `
-const HeaderTwo = styled.h2`
-  margin: 15px 0px;
-  text-align: center;
-  font-size: 24px;
-  letter-spacing: 3px;
-  line-height: 1.6;
-  font-family: 'Ubuntu Condensed', sans-serif;
-  color: rgb(82,150,250)
-`
-const AppreciatedEP = styled.div`
+const Tabs = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  margin-bottom: 20px;
-  color: #e8eddf
-`
-const EPArt = styled.img`
-  width: 400px;
-
-  @media(max-width: 415px){
-    width: 300px;
-  }
-`
-const StoreSection = styled.div`
-  display: flex;
-  flex-wrap: wrap;
   justify-content: center;
-  align-items: center;
-  margin-top: 25px;
+  width: 400px;
 `
-const StoreLnk = styled.a`
-  margin: 10px 20px;
-`
-const StoreLogo = styled.img`
-  border-radius: 10px;
-  width: 200px;
-  transition: transform .5s;
+const Tab = styled.div`
+  width: 50%;
+  background-color: #e8eddf;
+  border-left: ${({ barLeft }) => barLeft ? 'solid rgb(82,150,250) 2px' : null};
+  cursor: pointer;
 
-  &:hover{
-    transform: scale(1.25)
+  &:active{
+    transform: scale(.94)
   }
 `
-const ListingSection = styled.section`
-  margin: 1em 0 8em 0;
-  border-radius: 10px;
-  padding: 5px;
-`
-const ToolBelt = styled.div`
-  display: flex;
-  
-  @media(max-width: 770px){
-    flex-direction: column;
-  }
-`
-const CDCover = styled.img`
-  width: 300px;
-`
-const DividerArt = styled.hr`
-  margin: 15px;
-  width: 8%
-`
+
+
 export default function MyArt({ changeRole }) {
+
+  const [tabs, updateTabs] = useState({
+    music: true,
+    paint: false
+  })
 
   useEffect(() => {
     if (window.location.pathname === '/myart') {
@@ -97,49 +54,31 @@ export default function MyArt({ changeRole }) {
     }
   })
 
-
+  const handleClick = (tabName) => {
+    updateTabs({
+      music: false,
+      paint: false
+    })
+    updateTabs( prevTabs => ({
+      ...prevTabs,
+      [tabName]: !prevTabs[tabName]
+    }))
+  }
 
   return (
     <div>
       <ArtContainer>
-
-        <ArtSection>
-          <Header>My Art</Header>
-          <HeaderTwo>Listen to my "Appreciated" EP</HeaderTwo>
-          <AppreciatedEP>
-            <EPArt src='https://i.imgur.com/chpYvXn.png' alt='appreciated ep' />
-
-            <StoreSection>
-              <StoreLnk href='https://itunes.apple.com/us/album/appreciated-ep/id1238965226' target='_blank' rel='noopener noreferrer'>
-                <StoreLogo src="https://imgur.com/jeQ6reJ.png" alt="itunes" />
-              </StoreLnk>
-              <StoreLnk href='https://listen.tidal.com/album/74248583' target='_blank' rel='noopener noreferrer'>
-                <StoreLogo src="https://imgur.com/DoMBglI.png" alt="tidal" />
-              </StoreLnk>
-              <StoreLnk href='https://open.spotify.com/album/7zEyKnrn9ZbcL08pWDRavb' target='_blank' rel='noopener noreferrer'>
-                <StoreLogo src="https://imgur.com/k3G2C5e.png" alt="spotify" />
-              </StoreLnk>
-              <StoreLnk href='https://play.google.com/store/music/album/Stefon_Simmons_Appreciated?id=Bi6k4veuseh4xvacyfia2smp45e' target='_blank' rel='noopener noreferrer'>
-                <StoreLogo src="https://imgur.com/6XTu30o.png" alt="google play" />
-              </StoreLnk>
-              <StoreLnk href='https://amzn.to/2YMeF6p' target='_blank' rel='noopener noreferrer'>
-                <StoreLogo src="https://imgur.com/GJpA6K5.png" alt="amazon music" />
-              </StoreLnk>
-            </StoreSection>
-
-          </AppreciatedEP>
-
-        </ArtSection>
-        <DividerArt />
-        <ListingSection>
-          <HeaderTwo>Appreciated Track Listing</HeaderTwo>
-          <ToolBelt>
-            <CDCover src='https://imgur.com/AhVFued.png' alt='appreciated front cover' />
-            <CDCover src='https://imgur.com/9LVYiwt.png' alt='appreciated back cover' />
-            <CDDescription />
-          </ToolBelt>
-        </ListingSection>
-
+        <Header>My Art</Header>
+        <Tabs>
+          <Tab onClick={()=> handleClick('music')}>
+            <Header>Music</Header>
+          </Tab>
+          <Tab barLeft={true} onClick={()=> handleClick('paint')}>
+            <Header>Painting</Header>
+          </Tab>
+        </Tabs>
+        {tabs.music && <Music />}
+        {tabs.paint && <Painting />}
       </ArtContainer>
 
     </div>
